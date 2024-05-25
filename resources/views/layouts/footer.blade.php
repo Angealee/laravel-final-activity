bc91c00ab9230f50c2e77f89dba419c0ccb0b1a1
Koby Macale <koby.macale24@gmail.com>
2024-05-26 00:37:57 +0800

    updated niceadmin

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer id="footer" class="footer">
+    <div class="copyright">
+        &copy; Copyright <strong><span>Laravel Finals</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+        Modified by <a href="#">Koby C. Macale</a>
+    </div>
+</footer>
