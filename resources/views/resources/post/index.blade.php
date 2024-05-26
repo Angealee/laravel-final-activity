e0967f1d8b0379956a4ac25343290ec17d5f7040
Koby Macale <koby.macale24@gmail.com>
2024-05-26 20:15:25 +0800

    added a confirmation before deleting/removing

--- resources/views/resources/post/index.blade.php
+++ resources/views/resources/post/index.blade.php
@@ -1,13 +1,11 @@
 <x-app-layout>
-            
     <div class="pagetitle">
         <h1>{{ __('Post') }}</h1>
         <nav>
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{ route('dashboard')}}">{{__('Dashboard')}}</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">{{ __('Dashboard') }}</a></li>
                 <li class="breadcrumb-item active">{{ __('Resource') }}</li>
                 <li class="breadcrumb-item active">{{ __('Post') }}</li>
-                
             </ol>
         </nav>
     </div>
@@ -23,22 +21,22 @@
                     </div>
                     <script>
                         document.addEventListener('DOMContentLoaded', function() {
-                            // Use setTimeout to hide the alert after 5 seconds
                             setTimeout(function() {
                                 var alertElement = document.getElementById('autoDismissAlert');
                                 if (alertElement) {
-                                    // Use Bootstrap's alert 'close' method to hide it
                                     var alert = new bootstrap.Alert(alertElement);
                                     alert.close();
                                 }
-                            }, 5000); // 5000 milliseconds = 5 seconds
+                            }, 5000);
                         });
                     </script>
                 @endif
                 <div class="card p-4">
                     <div class="card-body">
                         <div class="text-end">
-                            <a href="{{ route('post.create') }}"  type="button" class="btn btn-primary" ><i class="bi bi-file-earmark-plus-fill me-1  "></i> Add a New Options</a> 
+                            <a href="{{ route('post.create') }}" type="button" class="btn btn-primary">
+                                <i class="bi bi-file-earmark-plus-fill me-1"></i> Add a New Option
+                            </a>
                         </div>
                         <hr class="my-5">
                         <table class="table datatable">
@@ -46,7 +44,7 @@
                                 <tr>
                                     <th scope="col">Subject</th>
                                     <th scope="col">Post</th>
-                                    <th scope="col">Status</th>   
+                                    <th scope="col">Status</th>
                                     <th scope="col">Options</th>
                                 </tr>
                             </thead>
@@ -54,20 +52,26 @@
                                 @isset($posts)
                                     @foreach($posts as $post)
                                         <tr>
-                                            <td>{{$post -> subject}}</td>
-                                            <td>{{$post -> post}}</td>
-                                            <td>{{ ($post -> status == 1 ? 'Published':'Unpublished') }}</td>
+                                            <td>{{ $post->subject }}</td>
+                                            <td>{{ $post->post }}</td>
+                                            <td>{{ $post->status == 1 ? 'Published' : 'Unpublished' }}</td>
                                             <td>
-                                            <a href="{{ route('post.show', $post) }}" class="btn btn-dark m-1" fdprocessedid="sh46d8"><i class="bi bi-folder-symlink"></i></a>
-                                            <a href="{{ route('post.edit', $post) }}" type="button" class="btn btn-success m-1" fdprocessedid="sh46d8"><i class="bi bi-pencil-square"></i></a>
-                                            <form action="{{ route('post.destroy', $post->id) }}" method="post">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger m-1" fdprocessedid="sh46d8"><i class="bi bi-trash-fill"></i></button>
-                                            </form>
-                                            </td>                  
+                                                <a href="{{ route('post.show', $post) }}" class="btn btn-dark m-1">
+                                                    <i class="bi bi-folder-symlink"></i>
+                                                </a>
+                                                <a href="{{ route('post.edit', $post) }}" class="btn btn-success m-1">
+                                                    <i class="bi bi-pencil-square"></i>
+                                                </a>
+                                                <form id="delete-form-{{ $post->id }}" action="{{ route('post.destroy', $post->id) }}" method="post" style="display: inline;">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="button" class="btn btn-danger m-1" onclick="confirmDelete({{ $post->id }})">
+                                                        <i class="bi bi-trash-fill"></i>
+                                                    </button>
+                                                </form>
+                                            </td>
                                         </tr>
-                                    @endforeach   
+                                    @endforeach
                                 @endisset
                             </tbody>
                         </table>
@@ -77,4 +81,26 @@
         </div>
     </section>
 
+    <script>
+        function confirmDelete(postId) {
+            swal({
+                title: "Are you sure?",
+                text: "You will not be able to recover this post!",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes, delete it!",
+                cancelButtonText: "No, cancel!",
+                closeOnConfirm: false,
+                closeOnCancel: false
+            },
+            function(isConfirm){
+                if (isConfirm) {
+                    document.getElementById('delete-form-' + postId).submit();
+                } else {
+                    swal("Cancelled", "Your post is safe :)", "error");
+                }
+            });
+        }
+    </script>
 </x-app-layout>
